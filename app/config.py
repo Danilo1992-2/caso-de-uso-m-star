b42f09d2bc57ebc438a3d4699f398b8c2645c99c
Danilo <danilo.silva192@icloud.com>
2024-02-04 17:09:47 -0300

    resolvido problema de cors para localhost

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -7,4 +8,5 @@ Base = declarative_base()
 DATABASE_URL = "mysql+pymysql://root:shaman@localhost:3306/Contash"
 engine = create_engine(DATABASE_URL)
 app = Flask(__name__)
+CORS(app)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
