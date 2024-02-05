4b222639ab708441cb6bc6f275b88ecd8bba2082
Danilo <danilo.silva192@icloud.com>
2024-02-05 12:52:33 -0300

    banco

--- app/config.py
+++ app/config.py
@@ -3,9 +3,10 @@ from flask_cors import CORS
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
 Base = declarative_base()
-DATABASE_URL = "mysql+pymysql://root:shaman@localhost:3306/Contash"
+DATABASE_URL = os.environ['MYSQL_CONNECTION_STRING']
 engine = create_engine(DATABASE_URL)
 app = Flask(__name__)
 CORS(app)
