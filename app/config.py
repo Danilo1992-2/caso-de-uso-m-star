19c86de4d4609f25e31a47fe2f7aa09b4b6a898d
Danilo <danilo.silva192@icloud.com>
2024-02-06 12:46:27 -0300

    variavel banco

--- app/config.py
+++ app/config.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_cors import CORS
 from sqlalchemy.orm import sessionmaker
@@ -5,7 +7,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-DATABASE_URL = "mysql+pymysql://root:shaman@localhost:3306/Contash"
+DATABASE_URL = os.environ.get('DATABASE_URL')
 engine = create_engine(DATABASE_URL)
 app = Flask(__name__)
 CORS(app)
